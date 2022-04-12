00a72a8abfd51f878e309c3c00662d5cc059d836
Angelo Alves <angelobehance@gmail.com>
2022-04-12 16:07:02 -0300

    FEAT: enviando e deletando tasks com local storage

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 var arrayTasks = JSON.parse(localStorage.getItem("tasks")) || []
 
-arrayTasks.forEach( (elemento) =>{
+arrayTasks.forEach((elemento) => {
     console.log(elemento)
     addTaskOnScreen(elemento)
 })
-
 function addTaskOnScreen(task) {
     let id = `task-${task.id}`
     let idCheck = `check-${task.id}`
-    //verificação se o input está vazio
-        const taskItem = document.createElement('taskItem');
-        taskItem.id = id
-        taskItem.innerHTML = `  
+    const taskItem = document.createElement('taskItem');
+    taskItem.id = id
+    taskItem.innerHTML = `  
             <li class="task">
                     <div class="to-do">
                         <input id='${idCheck}' type="checkbox">
@@ -19,14 +17,19 @@ function addTaskOnScreen(task) {
                         <input id="lixeira" class="delete-task" type="button" onclick="delTask(${task.id})" value="X">
                     </div>
             </li>`
-        const ul = document.getElementById('ul-task')
-        ul.appendChild(taskItem)
-        document.getElementById(idCheck).checked = task.check
+    const ul = document.getElementById('ul-task')
+    ul.appendChild(taskItem)
+    document.getElementById(idCheck).checked = task.check
+}
+function removeTask(elemento) {
+    elemento.remove()
 }
 function delTask(id) {
     var myobj = document.getElementById(`task-${id}`);
-    arrayTasks.splice(id,1)
-    myobj.remove(); 
+    arrayTasks.splice(arrayTasks.findIndex(elemento => elemento.id === id), 1)
+    myobj.remove();
+    localStorage.setItem("tasks", JSON.stringify(arrayTasks))
+
 }
 //pegando o valor do input (tarefa a ser adicionada) e dando um addTask(input.value)
 function clickTask() {
@@ -34,13 +37,14 @@ function clickTask() {
     taskObject.text = document.getElementById("inputTask").value;
     taskObject.check = false
     taskObject.id = arrayTasks.length
-    if (taskObject.text == ""){
+
+    if (taskObject.text == "") {
         alert('Adicione alguma tarefa')
-    }else{
+    } else {
         addTaskOnScreen(taskObject)
         arrayTasks.push(taskObject)
 
-    localStorage.setItem("tasks", JSON.stringify(arrayTasks))
+        localStorage.setItem("tasks", JSON.stringify(arrayTasks))
 
     }
     //deixando o input vazio após o envio da tarefa
@@ -48,8 +52,8 @@ function clickTask() {
 
 }
 //envio da tarefa também com a tecla enter
-document.addEventListener('keypress', function(e){
-    if(e.which == 13){
-      clickTask();
+document.addEventListener('keypress', function (e) {
+    if (e.which == 13) {
+        clickTask();
     }
-  }, false);
+}, false);
